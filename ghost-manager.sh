79801e94ad85aa0cdc623ef77459f2c483442801
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-10 00:56:05 -0500

    Update ghost-manager.sh

--- ghost-manager.sh
+++ ghost-manager.sh
@@ -27,18 +27,10 @@ dist-check
 
 # Pre-Checks system requirements
 function installing-system-requirements() {
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+  if [ "$DISTRO" == "ubuntu" ] && { [ "$DISTRO_VERSION" == "16.04" ] && [ "$DISTRO_VERSION" == "18.04" ] && [ "$DISTRO_VERSION" == "20.04" ]; }; then
     if { [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v iptables)" ] || [ ! -x "$(command -v bc)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v zip)" ] || [ ! -x "$(command -v unzip)" ] || [ ! -x "$(command -v grep)" ] || [ ! -x "$(command -v awk)" ] || [ ! -x "$(command -v shuf)" ]; }; then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      if [ "$DISTRO" == "ubuntu" ]; then
         apt-get update && apt-get install iptables curl coreutils bc jq sed e2fsprogs zip unzip grep gawk iproute2 hostname systemd -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
-        yum update -y && yum install iptables curl coreutils bc jq sed e2fsprogs zip unzip grep gawk hostname systemd -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
-        pacman -Syu --noconfirm iptables curl bc jq sed zip unzip grep gawk iproute2 hostname systemd coreutils
-      elif [ "$DISTRO" == "alpine" ]; then
-        apk update && apk add iptables curl bc jq sed zip unzip grep gawk iproute2 hostname systemd coreutils
-      elif [ "$DISTRO" == "freebsd" ]; then
-        pkg update && pkg install curl jq zip unzip gawk
       fi
     fi
   else
@@ -52,12 +44,15 @@ installing-system-requirements
 
 # Global variables
 GHOST_PATH="/var/www/html"
+GHOST_DEVELOPMENT_CONFIG_PATH="$GHOST_PATH/config.development.json"
+GHOST_PRODUCTION_CONFIG_PATH="$GHOST_PATH/config.production.json"
 GHOST_MANAGER_PATH="$GHOST_PATH/ghost-manager"
+NGINX_GLOBAL_CONFIG="/etc/nginx/nginx.conf"
 
 function previous-ghost-installation() {
   if [ -d "$GHOST_PATH" ]; then
-    if [ ! -f "$GHOST_MANAGER_PATH" ]; then
-      echo "Another installation of Ghost was found."
+    if { [ -x "$(command -v ghost)" ] || [ -f "$GHOST_DEVELOPMENT_CONFIG_PATH" ] || [ -f "$GHOST_PRODUCTION_CONFIG_PATH" ]; }; then
+      echo "Another ghost installation has been discovered."
     fi
   fi
 }
