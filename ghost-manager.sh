03723b740ceaf56f422459f916b6b983cb329ac5
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-10 23:15:26 -0500

    Update ghost-manager.sh

--- ghost-manager.sh
+++ ghost-manager.sh
@@ -62,7 +62,7 @@ installing-system-requirements
 function previous-ghost-installation() {
   if [ -d "$GHOST_PATH" ]; then
     if [ ! -d "$GHOST_MANAGER_PATH" ]; then
-      if { [ -x "$(command -v ghost)" ] || [ -f "$GHOST_DEVELOPMENT_CONFIG_PATH" ] || [ -f "$GHOST_PRODUCTION_CONFIG_PATH" ]; }; then
+      if { [ -f "$GHOST_DEVELOPMENT_CONFIG_PATH" ] || [ -f "$GHOST_PRODUCTION_CONFIG_PATH" ]; }; then
         echo "Another ghost installation has been discovered."
         exit
       fi
